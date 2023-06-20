2b5b7902fc434d82c5ffc9685d36a1854c62ebad
Lucie <luciesteigleder@gmail.com>
2023-06-20 10:44:05 +0200

    working on the quantity

--- index.js
+++ index.js
@@ -4,10 +4,41 @@ function focusSearchBar() {
 }
 
 
+
 //___________________________________________shoppingCart functions
+
 let cartItemList = document.querySelector(".cartItemList")
-let numberofItems = 1
-let numberOfArticles = 3
+
+//increase quantity
+let buttonsAddQuantityList = document.querySelectorAll(".addQuantity")
+const increaseQuantity = (event) => {
+    currentQuantity = parseInt(event.target.previousElementSibling.innerText)
+    if (currentQuantity < 1) {
+        event.target.parentNode.parentNode.style.opacity = "1"
+    }
+    newQuantity = ++currentQuantity
+    event.target.previousElementSibling.innerText = newQuantity
+}
+
+//decrease quantity
+let buttonsMinusQuantityList = document.querySelectorAll(".minusQuantity")
+const decreaseQuantity = (event) => {
+    currentQuantity = parseInt(event.target.nextElementSibling.innerText)
+    if (currentQuantity > 1) {
+        newQuantity = --currentQuantity
+        event.target.nextElementSibling.innerText = newQuantity
+    } else {
+        event.target.nextElementSibling.innerText = 0
+        event.target.parentNode.parentNode.style.opacity = "0.5"
+    }
+}
+
+let buttonsRemoveArticle = document.querySelectorAll(".removeArticle")
+const deleteArticle = (event) => {
+    event.target.parentNode.parentNode.remove()
+}
+
+
 
 
 //CREATE A NEW ARTICLE
@@ -54,14 +85,18 @@ const generateNewArticleCart = () => {
     //button minus quantity
     minusQuantity = document.createElement("button")
     minusQuantity.classList.add("minusQuantity")
+    minusQuantity.addEventListener("click", decreaseQuantity)
     minusQuantityIcon = document.createElement("i")
+    minusQuantityIcon.style.pointerEvents = ('none')
     minusQuantityIcon.classList.add("fa-solid")
     minusQuantityIcon.classList.add("fa-minus")
 
     //button add quantity
     addQuantity = document.createElement("button")
     addQuantity.classList.add("addQuantity")
+    addQuantity.addEventListener("click", increaseQuantity)
     addQuantityIcon = document.createElement("i")
+    addQuantityIcon.style.pointerEvents = ('none');
     addQuantityIcon.classList.add("fa-solid")
     addQuantityIcon.classList.add("fa-plus")
 
@@ -69,6 +104,16 @@ const generateNewArticleCart = () => {
     currentQuantity = document.createElement("p")
     currentQuantity.classList.add("currentQuantity")
     currentQuantityValue = document.createTextNode("1")
+
+    //button delete article
+    removeArticle = document.createElement("button")
+    removeArticle.classList.add("removeArticle")
+    removeArticle.addEventListener("click", deleteArticle)
+    removeArticleIcon = document.createElement("i")
+    removeArticleIcon.style.pointerEvents = ('none')
+    removeArticleIcon.classList.add("fa-solid")
+    removeArticleIcon.classList.add("fa-trash-can")
+
 
     //final hr
     hr = document.createElement("hr")
@@ -79,10 +124,13 @@ const generateNewArticleCart = () => {
         minusQuantity.appendChild(minusQuantityIcon)
         addQuantity.appendChild(addQuantityIcon)
         currentQuantity.appendChild(currentQuantityValue)
+        removeArticle.appendChild(removeArticleIcon)
 
         itemQuantity.appendChild(minusQuantity)
         itemQuantity.appendChild(currentQuantity)
         itemQuantity.appendChild(addQuantity)
+        itemQuantity.appendChild(removeArticle)
+
 
         //Item elements
         itemName.appendChild(itemNameText)
@@ -113,35 +161,40 @@ let numberItemsShowed
 
 const addNewItemCart = () => {
     generateNewArticleCart()
-    numberItemsValue = document.querySelector(".numberItemsValue") //I put the assignation here so it's reset each time we use the function
-    numberItemsValue.forEach // find a way to make the number of articles on the quantity of each instead of just the number of different articles
+    
+    //numberItemsValue.forEach // find a way to make the number of articles on the quantity of each instead of just the number of different articles
     
 
-    numberItemsValue.textContent = document.querySelectorAll(".cartItem").length
-    numberItemsShowed = document.querySelectorAll(".cartItem").length
+    //numberItemsValue.textContent = document.querySelectorAll(".cartItem").length
+    //numberItemsShowed = document.querySelectorAll(".cartItem").length
+}
+addNewItemCart()
 
+
+
+let list = []
+let innerText = []
+let sumArticles
+const calculateNumberofItems = () => {
+    
+
+    articleQuantitiesPara = document.querySelectorAll(".currentQuantity")
+    list = Array.from(articleQuantitiesPara);
+    innertext = list.map(e => parseInt(e.innerText))
+
+    sumArticles = parseInt(0)
+    for (i=0 ; i < innertext.length+1 ; i++) {
+        sumArticles = sumArticles + parseInt(innertext[i])
+    }
+
+    return sumArticles
 }
 
 
-let buttonsAddQuantityList = document.querySelectorAll(".addQuantity")
-buttonsAddQuantityList.forEach(element => {
-    element.addEventListener("click", (event) => {
-        currentQuantity = event.target.previousElementSibling.innerText
-        currentQuantity++
-        event.target.previousElementSibling.innerText = currentQuantity
-    })
-})
+numberItemsValue = document.querySelector(".numberItemsValue") //I put the assignation here so it's reset each time we use the function
 
-let buttonsMinusQuantityList = document.querySelectorAll(".minusQuantity")
-buttonsMinusQuantityList.forEach(element => {
-    element.addEventListener("click", (event) => {
-        currentQuantity = event.target.nextElementSibling.innerText
-        currentQuantity--
-        event.target.nextElementSibling.innerText = currentQuantity
-    })
-})
 
-let buttonsRemoveItem = document.querySelectorAll(".removeItems")
+
 
 
 //delete the item function: if it goes lower than 0, we should remove it
